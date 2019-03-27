20c3d7811b3c2498d555a834b3ffc0d310c8509e
Jérémy Foucault <jeremfouc@orange.fr>
2019-03-27 16:21:44 +0100

    eoo

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Directive, OnInit,  HostBinding, HostListener } from '@angular/core';
 
 @Directive({
@@ -21,27 +20,3 @@ import { Directive, OnInit,  HostBinding, HostListener } from '@angular/core';
   }
   
 }
-=======
-import {Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding} from '@angular/core';
-
-@Directive({
-selector: '[appHighlight]'
-})
-export class HighlightDirective implements OnInit {
-
-@HostBinding('style.backgroundColor') myBackgroundColor:string;
-constructor() { }
-
-ngOnInit() {
-}
-
-@HostListener('mouseenter') mouseEnterEvent(eventData:Event) {
-this.myBackgroundColor = 'yellow';
-}
-
-@HostListener('mouseleave') mouseLeaveEvent(eventData:Event) {
-this.myBackgroundColor = 'transparent';
-}
-
-}
->>>>>>> 1d6411349a5f6d612701c2546e7216ead4f00756
